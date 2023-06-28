40036e51b0060e3bc84b91f3804e1356fa13d2c1
Christine Lemmer-Webber <cwebber@dustycloud.org>
2023-06-28 12:00:49 -0400

    Export type getters

--- module/wasm/types.scm
+++ module/wasm/types.scm
@@ -20,36 +20,9 @@
 ;;; Code:
 
 (define-module (wasm types)
+  #:use-module (srfi srfi-9)
   #:use-module (ice-9 match)
-  #:export (<wasm> make-wasm
-            <param> make-param
-            <func-sig> make-func-sig
-            <type-use> make-type-use
-            <ref-type> make-ref-type
-            <array-type> make-array-type
-            <field> make-field
-            <struct-type> make-struct-type
-            <sub-type> make-sub-type
-            <rec-group> make-rec-group
-            <limits> make-limits
-            <table-type> make-table-type
-            <mem-type> make-mem-type
-            <global-type> make-global-type
-            <type> make-type
-            <import> make-import
-            <export> make-export
-            <mem-arg> make-mem-arg
-            <elem> make-elem
-            <data> make-data
-            <tag> make-tag
-            <local> make-local
-            <func> make-func
-            <table> make-table
-            <memory> make-memory
-            <global> make-global
-            <custom> make-custom
-
-            find-type))
+  #:export (find-type))  ; the rest are implicitly exported
 
 (define-syntax define-simple-record-type
   (lambda (x)
@@ -60,10 +33,22 @@
        (let ((stem (let ((str (symbol->string (syntax->datum #'<rt>))))
                      (string->symbol
                       (substring str 1 (1- (string-length str)))))))
-         (with-syntax ((make-rt (id-append #'<rt> 'make- stem)))
+         (with-syntax ((make-rt (id-append #'<rt> 'make- stem))
+                       (pred (id-append #'<rt> stem '?))
+                       ((getter ...) (map (lambda (f)
+                                            (id-append #'<rt> stem '-
+                                                       (syntax->datum f)))
+                                          #'(field ...))))
            #'(begin
-               (define <rt> (make-record-type '<rt> '(field ...)))
-               (define make-rt (record-constructor <rt>)))))))))
+               (define-record-type <rt>
+                 (make-rt field ...)
+                 pred
+                 (field getter)
+                 ...)
+               (export <rt>)
+               (export make-rt)
+               (export pred)
+               (export getter) ...)))))))
 (define-syntax-rule (define-simple-record-types (rt field ...) ...)
   (begin
     (define-simple-record-type rt field ...)

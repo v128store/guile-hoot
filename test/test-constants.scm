4478d341d3a10e10af906496892ef7720e5ed96e
David Thompson <dthompson2@worcester.edu>
2023-08-18 19:02:15 -0400

    test: Allow running test-constants against our VM.

--- test/test-constants.scm
+++ test/test-constants.scm
@@ -20,7 +20,9 @@
 ;;; Code:
 
 (use-modules (wasm assemble)
+             (wasm parse)
              (hoot compile)
+             (hoot reflect)
              (ice-9 binary-ports)
              (ice-9 match)
              (ice-9 popen)
@@ -29,6 +31,7 @@
 
 (define d8 (or (getenv "D8") "d8"))
 (define srcdir (or (getenv "SRCDIR") (getcwd)))
+(define use-hoot-vm? (equal? (getenv "USE_HOOT_VM") "1"))
 
 (define (scope-file file-name)
   (string-append srcdir "/" file-name))
@@ -66,13 +69,13 @@
     (close-port port)
     (string-trim-both output)))
 
-(define (compile-scheme-then-load-wasm-in-d8 constant)
+(define (compile-value/d8 constant)
   (call-with-compiled-wasm-file
    (compile constant)
    (lambda (wasm-file-name)
      (run-d8 (scope-file "test/load-wasm-and-print.js") "--" srcdir wasm-file-name))))
 
-(define (compile-call form)
+(define (compile-call/d8 form)
   (let lp ((form form) (files '()) (first? #t))
     (match form
       (()
@@ -83,10 +86,112 @@
         (lambda (file)
           (lp form (cons file files) #f)))))))
 
+(define reflect-wasm
+  (call-with-input-file "js-runtime/reflect.wasm" parse-wasm))
+
+;; Imitate d8's output for now.
+(define (print-values . vals)
+  (define (d8-format x)
+    (match x
+      (#t (display "true"))
+      (#f (display "false"))
+      ((and (? number?) (? inexact?))
+       (write
+        (if (= x (truncate x))
+            (inexact->exact x)
+            x)))
+      ((? eof-object?) (display "#eof"))
+      ((? hoot-complex?)
+       (d8-format (hoot-complex-real x))
+       (display "+")
+       (d8-format (hoot-complex-imag x))
+       (display "i"))
+      ((? hoot-fraction?)
+       (d8-format (hoot-fraction-num x))
+       (display "/")
+       (d8-format (hoot-fraction-denom x)))
+      ((or (? hoot-pair?) (? mutable-hoot-pair?))
+       (display "(")
+       (d8-format (hoot-pair-car x))
+       (let loop ((cdr (hoot-pair-cdr x)))
+         (match cdr
+           (() #t)
+           ((or (? hoot-pair?) (? mutable-hoot-pair?))
+            (display " ")
+            (d8-format (hoot-pair-car cdr))
+            (loop (hoot-pair-cdr cdr)))
+           (x
+            (display " . ")
+            (d8-format x))))
+       (display ")"))
+      ((or (? hoot-vector?) (? mutable-hoot-vector?))
+       (let ((k (hoot-vector-length x)))
+         (display "#(")
+         (unless (= k 0)
+           (do ((i 0 (+ i 1)))
+               ((= i (- k 1)))
+             (d8-format (hoot-vector-ref x i))
+             (display " "))
+           (d8-format (hoot-vector-ref x (- k 1))))
+         (display ")")))
+      ((or (? hoot-bytevector?) (? mutable-hoot-bytevector?))
+       (let ((k (hoot-bytevector-length x)))
+         (display "#vu8(")
+         (unless (= k 0)
+           (do ((i 0 (+ i 1)))
+               ((= i (- k 1)))
+             (display (hoot-bytevector-ref x i))
+             (display " "))
+           (display (hoot-bytevector-ref x (- k 1))))
+         (display ")")))
+      ((or (? hoot-bitvector?)
+           (? mutable-hoot-bitvector?))
+       (let ((k (hoot-bitvector-length x)))
+         (display "#*")
+         (do ((i 0 (+ i 1)))
+             ((= i k))
+           (display (hoot-bitvector-ref x i)))))
+      ((? string?)
+       (display x))
+      ((? mutable-hoot-string?)
+       (display (mutable-hoot-string->string x)))
+      ((? hoot-procedure?)
+       (display "#<procedure>"))
+      ((? hoot-symbol?)
+       (display (hoot-symbol-name x)))
+      ((? hoot-keyword?)
+       (format #t "#:~a" (hoot-keyword-name x)))
+      (_ (write x))))
+  (string-trim-both
+   (with-output-to-string
+     (lambda ()
+       (for-each (lambda (x)
+                   (d8-format x)
+                   (newline))
+                 vals)))))
+
+(define (compile-value/hoot expr)
+  (call-with-values (lambda () (compile-value reflect-wasm expr))
+    print-values))
+
+(define (compile-call/hoot form)
+  (call-with-values (lambda () (apply compile-call reflect-wasm form))
+    print-values))
+
+(define (compile-value* expr)
+  (if use-hoot-vm?
+      (compile-value/hoot expr)
+      (compile-value/d8 expr)))
+
+(define (compile-call* form)
+  (if use-hoot-vm?
+      (compile-call/hoot form)
+      (compile-call/d8 form)))
+
 (define-syntax-rule (test-compilation expr repr)
-  (test-equal repr repr (compile-scheme-then-load-wasm-in-d8 'expr)))
+  (test-equal repr repr (compile-value* 'expr)))
 (define-syntax-rule (test-call repr proc arg ...)
-  (test-equal repr repr (compile-call '(proc arg ...))))
+  (test-equal repr repr (compile-call* '(proc arg ...))))
 
 (test-compilation 42 "42")
 (test-compilation 100 "100")
